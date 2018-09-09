50cb0ee2551290d95e26aa2e3d7ccd05d064874e
dsreddy22 <32877301+dsreddy22@users.noreply.github.com>
2018-09-09 19:51:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,8 +13,3 @@ variable "app_service_plan_id" {
   description = "The id will come from the appservice plan"
   default = "/subscriptions/84adf927-45fd-4ca8-82a5-59ccda2785f5/resourceGroups/testwebapp83/providers/Microsoft.Web/serverFarms/serviceplan1"
 }
-variable "site_config" {
-  description = "A key-value pair for Site Config"
-  type        = "list"
-  default = []
-}
