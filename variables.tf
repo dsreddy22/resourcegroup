67d61f046a6296c47307cc6edc8f6f9419bd366d
dsreddy22 <32877301+dsreddy22@users.noreply.github.com>
2018-09-09 20:31:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,10 @@
 variable "resource_group" {
   type = "string"
   description = "The name of the resource group in which to create the app service and web apps."
-  default = "testwebapp83"
+  default = "testwebapp84"
 }
 variable "location" {
   type = "string"
   description = "The location/region where the web apps to be created."
   default = "centralus"
-}
-variable "app_service_plan_id" {
-  type = "string"
-  description = "The id will come from the appservice plan"
-  default = "/subscriptions/84adf927-45fd-4ca8-82a5-59ccda2785f5/resourceGroups/testwebapp83/providers/Microsoft.Web/serverFarms/serviceplan1"
-}
+  }
